b738945d6038a6edb692b6b3565252db82c8882c
CesarBoria <cesar510511@gmail.com>
2021-10-16 16:14:00 +0200

    The same thing, but using the kivy language.

--- sandbox.py
+++ sandbox.py
@@ -1,30 +1,35 @@
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.core.window import Window
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
-from kivy.uix.scatter import Scatter
-from kivy.uix.textinput import TextInput
+
+
+Builder.load_string('''
+<Label>:
+    font_size: 150
+<ScatterWidget>:
+    BoxLayout:
+        orientation: 'vertical'
+        FloatLayout:
+            TextInput:
+                id: input_written
+                text: 'Default'
+                size_hint_y: 0.2
+                multiline: False
+                font_size: self.height/2
+        Scatter:
+            pos: 100, 100
+            Label:
+                text: input_written.text
+''')
+
+
+class ScatterWidget(BoxLayout):
+    pass
 
 
 class TutorialApp(App):
     def build(self):
-        b = BoxLayout(orientation='vertical')
-        t = TextInput(text='Default', font_size=40, multiline=False, size_hint_y=0.1)
-        f = FloatLayout()
-        s = Scatter(center=(Window.width/2, Window.height/2))
-        l = Label(text='Default', font_size=50, color=(1, 1, 0, 1))
-        t.bind(text=l.setter('text'))
-
-        f.add_widget(s)
-        s.add_widget(l)
-
-        b.add_widget(f)
-        b.add_widget(t)
-
-        return b
+        return ScatterWidget()
 
 
 TutorialApp().run()
