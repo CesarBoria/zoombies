43fc42f52e6d8df90569711eeabaf1e9ab939602
CesarBoria <cesar510511@gmail.com>
2021-10-16 16:27:39 +0200

    Using ids to connect the kivy language with the python language.

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,4 @@
+from random import random
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
@@ -16,15 +17,20 @@ Builder.load_string('''
                 size_hint_y: 0.2
                 multiline: False
                 font_size: self.height/2
+                on_text: root.change_the_color()
         Scatter:
             pos: 100, 100
             Label:
+                id: label1
                 text: input_written.text
 ''')
 
 
 class ScatterWidget(BoxLayout):
-    pass
+    def change_the_color(self, *args):
+        label = self.ids['label1']
+        label.color=[random() for _ in range(3)] + [1]
+        print(label.text)
 
 
 class TutorialApp(App):
